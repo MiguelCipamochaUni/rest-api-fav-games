2afc7d6a7a1f3a5e6ecd203cdd8c54cca168a59c
MiguelCipamochaUni <160296462+MiguelCipamochaUni@users.noreply.github.com>
2024-08-23 21:41:49 -0500

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.set("port", process.env.PORT || 3000);
 app.set("json spaces", 2);
@@ -9,6 +10,7 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use(require("./routes/index"));
